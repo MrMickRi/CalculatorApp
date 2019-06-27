c0624bd5243b5130ccce95897c66ca8599951f31
MurphyM <MurphyM@IEDUB815856.dnbint.net>
2019-06-27 09:43:24 +0100

    Initial Commit

---
+++ Calculator/src/calculatorApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author MurphyM
+ *
+ */
+public class calculatorApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("YOLO");
+	}
+
+}
